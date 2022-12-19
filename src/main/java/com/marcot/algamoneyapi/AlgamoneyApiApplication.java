cbf4aa1206f1a5ba0b9eb10151f9fd65e7946176
MAntonioST <marco.teixeira4@fatec.sp.gov.br>
2022-12-19 11:20:55 -0300

    Projeto iniciado

---
+++ src/main/java/com/marcot/algamoneyapi/AlgamoneyApiApplication.java
@@ -0,0 +1,14 @@
+package com.marcot.algamoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AlgamoneyApiApplication.class, args);
+	}
+
+}
