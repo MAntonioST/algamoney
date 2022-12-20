7c701d35dbd3a112f68e00b815c108fb82fdaf9f
MAntonioST <marco.teixeira4@fatec.sp.gov.br>
2022-12-20 04:54:34 -0300

    Categoria Resources

--- src/main/java/com/marcot/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/marcot/algamoneyapi/resource/CategoriaResource.java
@@ -2,11 +2,14 @@ package com.marcot.algamoneyapi.resource;
 
 import com.marcot.algamoneyapi.model.Categoria;
 import com.marcot.algamoneyapi.repository.CategoriaRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,5 +22,22 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> listar(){
         return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria entity, HttpServletResponse response){
+        Categoria categoriaSalva = categoriaRepository.save(entity);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location",uri.toASCIIString());
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+        return this.categoriaRepository.findById(codigo)
+                .map(categoria -> ResponseEntity.ok(categoria))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
