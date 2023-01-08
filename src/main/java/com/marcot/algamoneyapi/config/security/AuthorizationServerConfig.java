392c340a518913d1e8fefc89223ac22cd2437943
MAntonioST <marco.teixeira4@fatec.sp.gov.br>
2023-01-08 12:33:16 -0300

    Add refresh token

--- src/main/java/com/marcot/algamoneyapi/config/security/AuthorizationServerConfig.java
+++ src/main/java/com/marcot/algamoneyapi/config/security/AuthorizationServerConfig.java
@@ -1,4 +1,4 @@
-package com.example.algamoney.api.config;
+package com.marcot.algamoneyapi.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
